8c36ed49e4653a2b8a46ffefea68c5f2b0d0372e
shashank-crypto <55027786+shashank-crypto@users.noreply.github.com>
2020-10-17 00:12:47 +0530

    Update contact_process.php

--- estore/contact_process.php
+++ estore/contact_process.php
@@ -9,7 +9,7 @@
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
+	$headers .= "Reply-To: ". $to . "\r\n";
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
